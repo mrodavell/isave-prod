99b5784ad6b46f3b14ae3ea287bc4c0bda7c46e5
mrodavelll <jomar.llevado@ustp.edu.ph>
2022-09-01 09:27:16 +0800

    For demo

---
+++ static/js/44.2520b2f7.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpisave=this.webpackJsonpisave||[]).push([[44],{715:function(e,i,s){"use strict";s.r(i),s.d(i,"default",(function(){return c}));var t=s(5),n=s(134),a=s(6);function c(e){return Object(a.jsx)("div",{style:{width:"100%",height:"100%"},children:Object(a.jsx)(t.Suspense,{fallback:Object(a.jsx)(n.a,{isDrawerOpen:e.open,isLoading:!0,zindex:2,opacity:.5})})})}}}]);
+//# sourceMappingURL=44.2520b2f7.chunk.js.map
